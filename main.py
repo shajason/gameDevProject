5dbf01b0ac1eab004c17e83d1d97d687922c752b
shajason <sjason@codio.com>
2024-03-28 19:55:10 +0000

    Add new feature in master

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ def main():
     print("Hello, welcome to our game!")
     print("We will bring you something exciting, soon!")
     print("Enjoy your adventure!")
+    print("New feature in master branch")
 
 if __name__ == "__main__":
     main()
