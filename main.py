6100107c90d1e1afcb6878f473afe3a5eb8e3a43
shajason <sjason@codio.com>
2024-03-28 19:51:28 +0000

    Added welcome message.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 def main():
     print("Hello, welcome to our game!")
     print("We will bring you something exciting, soon!")
+    print("Enjoy your adventure!")
 
 if __name__ == "__main__":
     main()
