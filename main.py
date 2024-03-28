0403760188a44f5f4b4276fd6e76955c371a1b51
shajason <sjason@codio.com>
2024-03-28 20:23:47 +0000

    Added player choice feature.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def main():
     print("We will bring you something exciting, soon!")
     print("Enjoy your adventure!")
     print("Conflicting feature in welcome-feature branch")
+    print("Please choose your player.")
 
 if __name__ == "__main__":
     main()
